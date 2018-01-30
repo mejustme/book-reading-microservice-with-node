f92072e34ff14f1393d44d67beee07bc62d0dec6
陈钦辉 <chenqinhui@didichuxing.com>
2018-01-30 11:08:49 +0800

    @dev update

--- http-server.js
+++ http-server.js
@@ -1,10 +1,10 @@
-var http = require('http')
-var server = http.createServer(function (req, res) {
+const http = require('http')
+const server = http.createServer(function (req, res) {
   res.setHeader('Set-Cookie', ['name=cqh', 'phone=iphone'])
   // res.setHeader('Location', 'http://www.if-elseif-else.com/')
   // res.writeHead(302, { 'Content-Type': 'text/plain' })
   res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.write('hello world')
+  res.write('hello world 100')
   res.end()
 })
 
